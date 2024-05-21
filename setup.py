6210db86d757b0ae4b93d8301370864aee11c061
deepak <honestharry1980@gmail.com>
2024-05-21 10:12:19 +0530

    folder uploaded

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup , find_packages
+setup( 
+    name='mcqgenerator', 
+    version='0.0.1', 
+    # description='A sample Python package', 
+    author='Deepak Yadav', 
+    author_email='honestharry1945@gmail.com', 
+    packages=find_packages(), 
+    install_requires=[ 
+        'numpy', 
+        'pandas', 
+        'openai',
+        'langchain',
+        "streamlit",
+        'python-dotenv',
+        'PyPDF2'
+    ], 
+)
