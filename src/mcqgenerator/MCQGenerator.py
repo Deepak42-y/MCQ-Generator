16aea22b07df8ed920166716517dd96945f5b124
deepak <honestharry1980@gmail.com>
2024-05-21 15:30:23 +0530

    streamlit

--- src/mcqgenerator/MCQGenerator.py
+++ src/mcqgenerator/MCQGenerator.py
@@ -2,44 +2,49 @@ import os
 import json
 import pandas as pd
 import traceback
-from src.mcqgenerator.utils import read_file, get_table_data
+from dotenv import load_dotenv
 from src.mcqgenerator.logger import logging
 
-
-from tabnanny import verbose
+#importing necessary packages from langchain
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
 from langchain.callbacks import get_openai_callback
-import PyPDF2
+import warnings
+warnings.filterwarnings('ignore')
 
-from dotenv import load_dotenv
 
-
+# Loading the environment variables from the .env file 
 load_dotenv()
-KEY = os.getenv("OPEN_API_KEY")
-llm = ChatOpenAI(openai_api_key = KEY, model_name = "gpt-3.5-turbo", temperature = 0.5)
+
+# Access the environment variables 
+KEY = os.getenv('OPEN_API_KEY')
+
+llm = ChatOpenAI(openai_api_key=KEY, model_name='gpt-3.5-turbo', temperature='0.5')
+
 
 
 TEMPLATE="""
-Text:{text}
+Text{text}
 You are an expert MCQ maker. Given the above text, it is your job to \
-create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
-Make sure the questions are not repeated and check all the questions to be conforming the text as well.
-Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
+create a quiz of {number} multiple choice questions for {subject} students in {tone} tone.
+Make sute the questions are not repeated and check all the questions to be confirmed the text as well.
+Make sure to format your response like RESPONSE_JSON below and use it as a guide. \
 Ensure to make {number} MCQs
 ### RESPONSE_JSON
 {response_json}
 
 """
 
-quiz_generation_prompt = PromptTemplate(
-    input_variables=["text", "number", "subject", "tone", "response_json"],
+quize_generation_prompt = PromptTemplate(
+    input_variables=['text','number','subject','tone','response_json'],
     template=TEMPLATE
-    )
+)
 
-quiz_chain = LLMChain(llm=llm , prompt=quiz_generation_prompt, output_key='quiz', verbose=True)
+quiz_chain = LLMChain(llm=llm, prompt=quize_generation_prompt, output_key='quiz', verbose=True)
+
 
 TEMPLATE2="""
 You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
@@ -52,14 +57,18 @@ Quiz_MCQs:
 Check from an expert English Writer of the above quiz:
 """
 
-quiz_evaluation_prompt=PromptTemplate(input_variables=["subject", "quiz"], template=TEMPLATE)
+quize_evaluation_prompt = PromptTemplate(
+    input_variables=['subject','quiz'], 
+    template=TEMPLATE2
+)
 
-review_chain=LLMChain(llm=llm, prompt=quiz_evaluation_prompt, output_key="review", verbose=True)
-
-# connect both chains by using sequential chain
-generate_evaluate_chain=SequentialChain(chains=[quiz_chain, review_chain], input_variables=["text", "number", "subject", "tone", "response_json"],
-                                        output_variables=["quiz", "review"], verbose=True,)
-
+review_chain = LLMChain(llm=llm, prompt=quize_evaluation_prompt, output_key='review', verbose=True)
 
 
-
+# Combining both quiz_chain and review_chain into a single chain  
+generate_evaluate_chain = SequentialChain(
+    chains=[quiz_chain, review_chain], 
+    input_variables=['text','number','subject','tone','response_json'],
+    output_variables=['quiz','review'],
+    verbose=True
+)
--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -1,46 +1,49 @@
-import os
-import PyPDF2
+import os 
 import traceback
+import PyPDF2
 import json
-
 
 def read_file(file):
     if file.name.endswith(".pdf"):
         try:
-            pdf_reader-PyPDF2.PdfFileReader(file)
-            text=""
+            pdf_reader = PyPDF2.PdfFileReader(file)
+            text = ""
             for page in pdf_reader.pages:
-                text+=page.extract_text()
-            return text
-
+                text += page.extract_text()
+                return text
+        
         except Exception as e:
-            raise Exception("error reading the PDF file")
-
+            raise Exception('Error reading the PDF file')
+    
     elif file.name.endswith(".txt"):
-        return file.read().decode("utf-8")
-
+        return file.read().decode('utf-8')
+    
     else:
         raise Exception(
-            "unsupported file format only pdf and text file suppoted" 
-            )
-    
+            'Unsupported file format only PDF and Text file supported.'
+        )
+
+
 def get_table_data(quiz_str):
     try:
-        # convert quiz from str to dict
-        quiz_dict = json.load(quiz_str)
+        #converting the quiz fro str to dict
+        quiz_dict=json.loads(quiz_str)
         quiz_table_data = []
 
-        for key, value in quiz_dict.items():
-            mcq = value["mcq"]
-            options = " | ".join(
+        #iterating over the quiz dictionary and extracting the required information
+        for key,value in quiz_dict.items():
+            mcq=value['mcq']
+            options = " || ".join(
                 [
-                    f"{option}: {option_value}"
-                    for option, option_value in value["options"].items()
-                    ]
-                )
-            correct = value["correct"]
-            quiz_table_data.append({"MCQ": mcq, "Choices": options, "Correct": correct})
+                    f"{option} -> {option_value}" for option, option_value in value['options'].items()
+                ]
+            )
+
+            correct=value['correct']
+            quiz_table_data.append({'MCQ':mcq, 'Choices':options, 'Correct':correct})
+        
         return quiz_table_data
+
     except Exception as e:
-            traceback.print_exception(type(e),e,e.__traceback__)
-            return False
+        traceback.print_exception(type(e), e, e.__traceback__)
+        return False 
